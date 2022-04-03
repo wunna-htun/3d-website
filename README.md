83292a377961cc3e20e82d79f199fc4270c7ad84
wunna-htun <wunnahtun@uit.edu.mm>
2022-04-04 02:42:31 +0630

    3D animated Website

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+## Description 3D Profolio Website 
+
+```
+3D Website with Three js 
+
+```
+
+
+
+## Demo 
+
+https://3dwebsite-001.netlify.app/
+
+
+
+
--- main.js
+++ main.js
@@ -5,37 +5,39 @@ import { AmbientLight } from 'three';
 
 const scene = new THREE.Scene();
 
-const camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 
-const renderer= new THREE.WebGLRenderer({
-  canvas:document.querySelector('#bg')
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
 })
 
 renderer.setPixelRatio(window.devicePixelRatio)
 
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
 
-renderer.render(scene,camera)
+renderer.render(scene, camera)
 
 
-const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material= new THREE.MeshStandardMaterial({color:0xFF6347});
-const torus =new THREE.Mesh(geometry,material);
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
 
-const pointLight =new THREE.PointLight(0xfffff)
-pointLight.position.set(5,5,5)
+// Lights
 
-const ambientLight=new THREE.AmbientLight(0xfffff)
-scene.add(pointLight,ambientLight)
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
 
-const lightHelper= new THREE.PointLightHelper(pointLight)
-const gridHelper= new THREE.GridHelper(200,50)
-scene.add(lightHelper,gridHelper)
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+// scene.add(lightHelper, gridHelper)
 
 
 
@@ -44,29 +46,94 @@ scene.add(lightHelper,gridHelper)
 
 
 
-function addStar(){
-  const geometry = new THREE.SphereGeometry(0.25,24,24);
-  const material= new THREE.MeshStandardMaterial({color:0xffffff})
-  const star = THREE.Mesh(geometry,material); 
+// Star
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
 
-  const [x,y,z] = Array(3).fill().map(()=>{
-    THREE.MathUtils.randFloatSpread(100)
-  })
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x,y,z);
+  star.position.set(x, y, z);
+  scene.add(star);
 }
 
+Array(200).fill().forEach(addStar);
 
-function animate(){
+
+// background
+
+const spaceTexture= new THREE.TextureLoader().load("/assets/space.jpg")
+scene.background=spaceTexture;
+
+
+// wunna
+const wunnaTexture = new THREE.TextureLoader().load("/assets/wunna.JPG")
+const wunna = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({
+    map:wunnaTexture
+  })
+
+)
+
+scene.add(wunna)
+
+
+
+
+const moonTexture = new THREE.TextureLoader().load('/assets/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+
+function moveCamera(){
+
+  const t=document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  wunna.rotation.y += 0.01;
+  wunna.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+
+
+}
+
+document.body.onscroll=moveCamera;
+moveCamera();
+
+
+
+
+function animate() {
 
   requestAnimationFrame(animate);
 
-  torus.rotation.y +=0.01;
-  torus.rotation.x +=0.005;
-  torus.rotation.z +=0.01;
+  torus.rotation.y += 0.01;
+  torus.rotation.x += 0.005;
+  torus.rotation.z += 0.01;
 
   // controls.update();
-  renderer.render(scene,camera);
+  renderer.render(scene, camera);
 
 }
 
